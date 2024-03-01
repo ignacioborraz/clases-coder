1ff56e5b9e41c80e615de0c3195cfa732e04865d
Tomas <arnotomas1@gmail.com>
2024-02-29 21:50:17 -0300

    CRUD categories added

--- src/routers/api/index.router.api.js
+++ src/routers/api/index.router.api.js
@@ -1,13 +1,16 @@
-import CustomRouter from "../CustomRouter.js";
-import ClothesRouter from "./clothes.router.api.js";
-import SessionsRouter from "./sessions.router.api.js";
+import CustomRouter from '../CustomRouter.js';
+import ClothesRouter from './clothes.router.api.js';
+import SessionsRouter from './sessions.router.api.js';
+import CategoriesRouter from './categories.router.api.js';
 
 const session = new SessionsRouter();
 const clothes = new ClothesRouter();
+const categories = new CategoriesRouter();
 
 export default class ApiRouter extends CustomRouter {
   init() {
-    this.use("/sessions", session.getRouter());
-    this.use("/clothes", clothes.getRouter());
+    this.use('/sessions', session.getRouter());
+    this.use('/clothes', clothes.getRouter());
+    this.use('/categories', categories.getRouter());
   }
 }
