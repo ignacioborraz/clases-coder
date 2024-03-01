15c1b0bf5c60e50526e3acf6db051cb7a2174ce4
igna <ignacioborraz@hotmail.com>
2024-02-29 21:13:21 -0300

    clothes router

--- src/routers/api/index.router.api.js
+++ src/routers/api/index.router.api.js
@@ -1,10 +1,13 @@
 import CustomRouter from "../CustomRouter.js";
+import ClothesRouter from "./clothes.router.api.js";
 import SessionsRouter from "./sessions.router.api.js";
 
-const session = new SessionsRouter()
+const session = new SessionsRouter();
+const clothes = new ClothesRouter();
 
 export default class ApiRouter extends CustomRouter {
   init() {
     this.use("/sessions", session.getRouter());
+    this.use("/clothes", clothes.getRouter());
   }
 }
