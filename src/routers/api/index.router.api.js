dc7c694f8f41328bda41d0e8561971a6f98c5cff
Gabriel Rodriguez <garodriguezm96@gmail.com>
2024-02-29 22:09:15 -0300

    Create sizes CRUD (#2)

--- src/routers/api/index.router.api.js
+++ src/routers/api/index.router.api.js
@@ -1,13 +1,16 @@
+import SizesRouter  from "./sizes.router.api.js";
 import CustomRouter from "../CustomRouter.js";
 import ClothesRouter from "./clothes.router.api.js";
 import SessionsRouter from "./sessions.router.api.js";
 
 const session = new SessionsRouter();
 const clothes = new ClothesRouter();
+const sizes = new SizesRouter();
 
 export default class ApiRouter extends CustomRouter {
   init() {
     this.use("/sessions", session.getRouter());
     this.use("/clothes", clothes.getRouter());
+    this.use("/sizes", sizes.getRouter())
   }
 }
